8fd8f126699be44ef34fa71c1b50c7b9fcf073a2
devops20jan <devops20jan@gmail.com>
2020-03-13 05:24:58 +0000

    test cases

---
+++ src/test/java/com/qshore/Qshore/LaunchApp.java
@@ -0,0 +1,16 @@
+package com.qshore.Qshore;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchApp {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://google.com");
+		driver.manage().window().maximize();
+
+	}
+
+}
